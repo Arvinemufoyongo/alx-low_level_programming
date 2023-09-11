6664e69de92012cea09ef9c9e56e7732f97f8d50
Arvinemufoyongo <arvinemufoyongo@gmail.com>
2023-09-11 13:46:01 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is my mainfunction
+ *
+ * Return: values is 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
