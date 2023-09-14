c24237f0d296c8e9a0914c21c63eeb628759a77d
Arvinemufoyongo <arvinemufoyongo@gmail.com>
2023-09-13 17:55:10 -0700

    ature made the natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,13 +6,14 @@
 
 int main(void)
 {
-	int sum = o;
+	int sum = 0;
 	int i;
 
-	for (i = 0 ; i < 1024 : i++)
+	for (i = 0 ; i < 1024 ; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum = sum + i;
 	}
 	printf("%d\n", sum);
+	return (0);
 }
